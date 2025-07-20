062e9eeddbc007651daff5d1e978c48df6dea986
ehsan <60423941+ehsan0203@users.noreply.github.com>
2025-07-20 21:06:46 +0330

    Cleaned up unnecessary tracked files using .gitignore

--- TennisAcademy.Application/DTOs/Auth/RefreshTokenDto.cs
+++
@@ -1,9 +0,0 @@
-namespace TennisAcademy.Application.DTOs.Auth
-{
-    public class RefreshTokenDto
-    {
-        public string Token { get; set; }
-        public string RefreshToken { get; set; }
-    }
-}
-
